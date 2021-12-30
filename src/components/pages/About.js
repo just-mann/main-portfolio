ac42bd82ab6e5eb23193a3060136cc67c97bdb4f
just-mann <justinkollie890@yahoo.com>
2021-12-29 20:00:29 -0500

    changes made

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BsInstagram, BsGithub, BsTwitter} from 'react-icons/bs';
 
 const About = () => {
     return (
@@ -6,19 +7,25 @@ const About = () => {
             <div className="aboutContainer">
                 <div className="aboutLeft">
                     <h3>About Me</h3>
-                    <h2>I'am a web developer & designer</h2>
+                    <h2>I am a Front-End Web Developer</h2>
                     <p>
                         Lorem ipsum dolor sit amet consectetur 
                         adipisicing elit. Quibusdam perspiciatis a rem in? 
-                        Magnam, aperiam laboriosam porro nobis illum ducimus harum, 
-                        culpa nisi animi sit quod obcaecati reprehenderit dolorem minima architecto? Totam, 
-                        vero! Voluptatibus doloremque provident dolores 
-                        exercitationem nam cumque?
+                        Magnam, aperiam laboriosam porro nobis illum ducimus harum?
                     </p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                         Iste eos numquam vero placeat obcaecati corrupti, quasi, 
                         molestiae sequi dolore itaque blanditiis libero deserunt cum labore!
                     </p>
+                    <div className="socialDiv">
+                        <h3>Follow Me</h3>
+                        <div>
+                            <a href='#' target='_blank' className='instagram'><BsInstagram /></a>
+                            <a href='#' target='_blank' className='github'><BsGithub /></a>
+                            <a href='#' target='_blank' className='instagram'><BsTwitter /></a>
+                        </div>
+                        {/* <i className='instagram'><BsInstagram /></i> */}
+                    </div>
                 </div>
                 <div className="aboutRight">
                     <img src="" alt="" />
