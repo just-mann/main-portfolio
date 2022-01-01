0998f3e5d98facc41344c7c2714e64b33030b5b3
just-mann <justinkollie890@yahoo.com>
2021-12-31 19:20:09 -0500

    changes made

--- src/components/pages/partials/Navbar.js
+++ src/components/pages/partials/Navbar.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className='Navbar'>
+            <div>
+                <a href='#' className="myLogo">
+                    <h3>justin</h3>
+                </a>
+                <div className="navLinks">
+                    <Link to='/' className='home'>Home</Link>
+                    <a href="#" className='about'>About</a>
+                    <Link to='/portfolio' className='work'>Work</Link>
+                    <Link to='/contact-me' className='contact'>Contact</Link>
+                </div>
+                <div className="menu">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
         </div>
     )
 }
