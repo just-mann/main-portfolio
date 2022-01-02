1adffe6d9a9c6ee26ebf58fd048257edde643792
just-mann <justinkollie890@yahoo.com>
2022-01-02 01:11:26 -0500

    edited and styled the home hero section

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './partials/Navbar';
 import bgImage from './images/myBgImg.png';
+import {Link} from 'react-router-dom';
 import {BsLinkedin, BsGithub, BsTwitter, BsInstagram} from 'react-icons/bs';
 
 const Home = () => {
@@ -9,24 +10,38 @@ const Home = () => {
         backgroundImage: `url(${bgImage})`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
-        width: '100%'
+        width: '100%',
+        minHeight: '90vh'
     }
 
     return (
         <div className='Home'>
             <div className="heroSection" style={myBackground}>
-                <Navbar />
-                
-                    <div className="heroSocial">
-                        <a href="#"><BsLinkedin /></a>
-                        <a href="#"><BsGithub /></a>
-                        <a href="#"><BsTwitter /></a>
-                        <a href="#"><BsInstagram /></a>
+                <div className="heroContainer">
+                    <Navbar />
+                    <div className="mydiv">
+                        <div className="heroSocial">
+                            <a href="#"><BsLinkedin /></a>
+                            <a href="#"><BsGithub /></a>
+                            <a href="#"><BsTwitter /></a>
+                            <a href="#"><BsInstagram /></a>
+                        </div>
+                        <div className="heroMain">
+                            <div className="heroMainLeft">
+                                <h3 className='mainHeading'>I am <span>Justin Kollie</span></h3>
+                                <h2 className='subHeading'>I am a front-end web developer</h2>
+                                <p className="description">
+                                    Lorem, ipsum. Accusantium quod libero quasi veritatis 
+                                    consectetur vel natus, facere modi doloribus non. 
+                                    Dolorum soluta aperiam vero.
+                                </p>
+                                <Link to='/' className="downloadResumeBtn">See Resume</Link>
+                                <Link to='/contact-me' className="heroContactBtn">Contact Me</Link>
+                            </div>
+                            <div className="heroMainRight"></div>
+                        </div>
                     </div>
-                    <div className="heroMain">
-                        
-                    </div>
-                
+                </div>
             </div>
         </div>
     )
