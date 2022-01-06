cdf1c8564911749997fd7b658e41bb6e121d1096
just-mann <justinkollie890@yahoo.com>
2022-01-06 15:35:20 -0500

    changes made

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,11 @@ import React, {useState, useEffect} from 'react';
 import Navbar from './partials/Navbar';
 import bgImage from './images/lineBg.jpeg';
 import {Link} from 'react-router-dom';
+import {FaReact, FaGit, FaGithub, FaNodeJs, FaFigma} from 'react-icons/fa';
+import {SiJquery, SiExpress, SiCsswizardry} from 'react-icons/si';
+import {BsBootstrap} from 'react-icons/bs';
+import {MdMobileFriendly} from 'react-icons/md';
+import {IoLogoJavascript, IoLogoSass} from 'react-icons/io';
 
 const Home = () => {
 
@@ -12,6 +17,10 @@ const Home = () => {
         backgroundSize: 'cover',
         objectFit: 'cover'
     }
+
+    const designImg = <FaFigma />;
+    const devImg = <FaFigma />;
+    const respDesImg = <MdMobileFriendly />;
 
     return (
         <div>
@@ -39,19 +48,47 @@ const Home = () => {
                 </div>
                 
             </section>
-            <section className="skills">
+            <section className="mySkills">
                 <h3>My Skills</h3>
                 <div className='skillsContainer'>
-                    <div className="skill"></div>
-                    <div className="skill"></div>
-                    <div className="skill"></div>
-                    <div className="skill"></div>
-                    <div className="skill"></div>
-                    <div className="skill"></div>
-                    <div className="skill"></div>
-                    <div className="skill"></div>
-                    <div className="skill"></div>
-                    <div className="skill"></div>
+                    <div className="skill" title='JavaScript'><IoLogoJavascript /></div>
+                    <div className="skill" title='ReactJS'><FaReact /></div>
+                    <div className="skill" title='NodeJS'><FaNodeJs /></div>
+                    <div className="skill" title='Sass'><IoLogoSass /></div>
+                    <div className="skill" title='Bootstrap4'><BsBootstrap /></div>
+                    <div className="skill" title='jQuery'><SiJquery /></div>
+                    <div className="skill" title='Git'><FaGit /></div>
+                    <div className="skill" title='Github'><FaGithub /></div>
+                    <div className="skill" title='Express'><SiExpress /></div>
+                    <div className="skill" title='CSS3'><SiCsswizardry /></div>
+                </div>
+            </section>
+            <section className="mySpecialty">
+                <div className="specContainer">
+                    <div className='respDesign'>
+                        <i>{designImg}</i>
+                        <h3>Creative Design</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos architecto omnis autem rem tempora. Aliquam.
+                        </p>
+                        <Link to='/'>Icon</Link>
+                    </div>
+                    <div className='development'>
+                        <i class="fas fa-code"></i>
+                        <h3>Development</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos architecto omnis autem rem tempora. Aliquam.
+                        </p>
+                        <Link to='/'>Icon</Link>
+                    </div>
+                    <div className='design'>
+                        <i>{respDesImg}</i>
+                        <h3>Mobile Friendly</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos architecto omnis autem rem tempora. Aliquam.
+                        </p>
+                        <Link to='/'>Icon</Link>
+                    </div>
                 </div>
             </section>
         </div>
