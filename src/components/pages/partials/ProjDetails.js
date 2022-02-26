a93793c51fc84c070c54caf469d08cf978d4242b
just-mann <justinkollie890@yahoo.com>
2022-02-26 17:01:45 -0500

    changes made

---
+++ src/components/pages/partials/ProjDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "./Navbar";
+
+
+
+const ProjDetails = () => {
+    return (
+        <div className="ProjDetails">
+            <Navbar />
+        </div>
+    );
+}
+ 
+export default ProjDetails;
